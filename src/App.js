49c72620f9ce7a6aee906ceabb5b1218caaf38ca
Zero <zeroto521@gmail.com>
2020-12-26 14:35:30 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ const App = () => {
       { createBlog() }
       {
         user &&
-        <BlogForm blogs={blogs} setBlogs={setBlogs} />
+        <BlogForm blogs={blogs} />
       }
     </div>
   )
