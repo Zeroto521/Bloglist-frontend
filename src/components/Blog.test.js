de3d171a387dac029ba7cd35449ff7a4caf87624
Zero <zeroto521@gmail.com>
2020-12-27 18:57:36 +0800

    :construction: doing 5.15

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,8 +1,11 @@
 import '@testing-library/jest-dom'
+import { prettyDOM } from '@testing-library/dom'
 import { render, fireEvent } from '@testing-library/react'
 import React from 'react'
 
 import Blog from './Blog'
+import blogService from '../services/blogs'
+import loginService from '../services/login'
 
 const blog = {
   'title': "React patterns",
@@ -10,6 +13,20 @@ const blog = {
   'url': "https://reactpatterns.com",
   'likes': 7
 }
+
+const testUser = {
+  'username': 'test',
+  'password': 'test',
+}
+
+beforeEach(async () => {
+  const user = await loginService.login(testUser)
+  window.localStorage.setItem(
+    'loggedBlogappUser', JSON.stringify(user)
+  )
+
+  blogService.setToken(user.token)
+})
 
 test('default view, can only see title and author', () => {
   const component = render(
@@ -24,7 +41,7 @@ test('default view, can only see title and author', () => {
 
 test('click view button and can see blog detail', () => {
   const component = render(
-    <Blog blog={blog} mockLikeChangeHandler />
+    <Blog blog={blog} />
   )
 
   const buttonView = component.getByText('view')
@@ -34,4 +51,26 @@ test('click view button and can see blog detail', () => {
   expect(blogAll).toBeVisible()
   expect(blogAll).toHaveTextContent(`${blog.url}`)
   expect(blogAll).toHaveTextContent(`${blog.likes}`)
+})
+
+
+test('click like button twice and likes will plus two', () => {
+  const mockSetBlogs = jest.fn()
+  const mockNotifyWith = jest.fn()
+
+  const component = render(
+    <Blog blog={blog} setBlogs={mockSetBlogs} notifyWith={mockNotifyWith} />
+  )
+
+  const buttonView = component.getByText('view')
+  fireEvent.click(buttonView)
+
+  const blogAll = component.container.querySelector('.blogAll')
+  expect(blogAll).toBeVisible()
+
+  const buttonLike = component.getByText('like')
+  fireEvent.click(buttonLike)
+  fireEvent.click(buttonLike)
+
+  console.log(prettyDOM(buttonLike))
 })
