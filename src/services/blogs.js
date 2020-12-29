f3b605ad4aed4f203e35728062f834cc174e2082
Zero <zeroto521@gmail.com>
2020-12-29 23:13:18 +0800

    Update blogs.js

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const baseUrl = '/api/blogs'
+// FIX Network Error for jest, jest can't read proxy config.
+const baseUrl = 'http://localhost:3001/api/blogs'
 
 let token = null
 
@@ -8,14 +9,14 @@ const setToken = newToken => {
   token = `bearer ${newToken}`
 }
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+const getAll = async () => {
+  const request = await axios.get(baseUrl)
+  return request.data
 }
 
-const getBlogByID = (id) => {
-  const request = axios.get(`${baseUrl}/${id}`)
-  return request.then(response => response.data)
+const getBlogByID = async (id) => {
+  const request = await axios.get(`${baseUrl}/${id}`)
+  return request.data
 }
 
 const create = async newObject => {
@@ -27,22 +28,22 @@ const create = async newObject => {
   return response.data
 }
 
-const update = (id, newObject) => {
+const update = async (id, newObject) => {
   const config = {
     'headers': { 'Authorization': token },
   }
 
-  const request = axios.put(`${baseUrl}/${id}`, newObject, config)
-  return request.then(response => response.data)
+  const request = await axios.put(`${baseUrl}/${id}`, newObject, config)
+  return request.data
 }
 
-const remove = (id) => {
+const remove = async (id) => {
   const config = {
     'headers': { 'Authorization': token },
   }
 
-  const request = axios.delete(`${baseUrl}/${id}`, config)
-  return request.then(response => response.data)
+  const request = await axios.delete(`${baseUrl}/${id}`, config)
+  return request.data
 }
 
 export default {
